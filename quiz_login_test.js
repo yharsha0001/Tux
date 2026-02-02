24fbc1a1e08bc719b43e4906c0d075365e13f159
yharsha0001 <118372461+yharsha0001@users.noreply.github.com>
2026-02-02 19:04:13 +0100

    Update quiz_login_test.js

--- quiz_login_test.js
+++ quiz_login_test.js
@@ -31,7 +31,7 @@ export default async function () {
 
   try {
     // Join quiz
-    await page.goto('https://alientux.com/join/431914', {
+    await page.goto('https://alientux.com/join/052374', {
       waitUntil: 'networkidle',
       timeout: 60000,
     });
