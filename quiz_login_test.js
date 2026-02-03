6eb88fa123d3232f1f158cedabf3bb18b48dd7bb
yharsha0001 <118372461+yharsha0001@users.noreply.github.com>
2026-02-03 18:18:10 +0100

    Update quiz_login_test.js

--- quiz_login_test.js
+++ quiz_login_test.js
@@ -8,7 +8,7 @@ export const options = {
   scenarios: {
     quizUsers: {
       executor: 'per-vu-iterations',
-      vus: 20,
+      vus: 30,
       iterations: 1,
       maxDuration: '6m',
       options: {
@@ -31,7 +31,7 @@ export default async function () {
 
   try {
     // Join quiz
-    await page.goto('https://alientux.com/join/904773', {
+    await page.goto('https://alientux.com/join/970840', {
       waitUntil: 'networkidle',
       timeout: 60000,
     });
