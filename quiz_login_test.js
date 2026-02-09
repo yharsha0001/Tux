b915872f3c119979c506351664d600917bbdf91c
yharsha0001 <118372461+yharsha0001@users.noreply.github.com>
2026-02-09 18:07:48 +0100

    Update quiz_login_test.js

--- quiz_login_test.js
+++ quiz_login_test.js
@@ -8,7 +8,7 @@ export const options = {
   scenarios: {
     quizUsers: {
       executor: 'per-vu-iterations',
-      vus: 20,               
+      vus: 15,               
       iterations: 1,
       maxDuration: '10m',
       options: {
